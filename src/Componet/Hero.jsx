9957a8cdb4f26cff1f607560b600de6156205d3a
au-lex <aulexc4d2@gmail.com>
2024-07-23 21:54:39 +0100

    correct

--- src/Componet/Hero.jsx
+++ src/Componet/Hero.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 
 export const Hero = () => {
   return (
-    <section className="relative pt-[4rem] px-4 bg-gradient-to-r from-[#fcf9de] to[#f3e9cc]"> {/* Updated gradient background */}
-      <div className="flex items-center justify-center text-center px-4">
+    <section className="relative pt-[4rem] px-4 bg-gradient-to-r from-[#f7f6f0] to[#f3e9cc]"> {/* Updated gradient background */}
+      <div className="flex items-center justify-center text-center">
         <section className="pt-[4rem]">
           <span className="text-yellow-500 uppercase tracking-[2px] text-[10px] mb-2 block">Endless Choices, One Click Away</span>
-          <h1 className="text-2xl md:text-6xl font-bold"> </h1>
-          <h1 className="text-2xl font-extrabold text-blu-500">
+         
+          <h1 className="text-[1.5rem] font-extrabold text-blu-500">
           Welcome <span className="text-yellow-500">Crypto_Cart</span>
   </h1>
           <p className="text-[12px] mt-2">Your digital marketplace for endless possibilities</p>
           <div className="">
-            <p className="text-[12px]">
+            <p className="text-[14px]">
               Discover a world of convenience, variety, and unbeatable deals. From fashion to tech, home essentials to unique finds, we've got it all under one virtual roof.
             </p>
           </div>
@@ -28,7 +28,7 @@ export const Hero = () => {
         </section>
       </div>
 
-      <div className="hero w-full borr rounded-sm shadow-sm">
+      <div className="hero w-full borr ">
         <img src="https://cdn3d.iconscout.com/3d/premium/thumb/online-shopping-delivery-8191228-6594626.png" alt="heroimg"
         className='w-[200%] h-[300px] object-cover'
         />
